a113318af681893b5a7a5b3555d0ae15c161e97a
Lenardski <lenardski@gmail.com>
2022-02-02 22:23:46 +0800

    Testing minor git push changes

---
+++ src/test/java/com/projectdemo/pos/controller/EmployeeControllerTest.java
@@ -0,0 +1,17 @@
+package com.projectdemo.pos.controller;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class EmployeeControllerTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+	
+	
+	
+
+}
